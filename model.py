e551343fc7b2712744ad6b5ca51abbf824ef417c
Sumit Mishra <46062583+SumitM0432@users.noreply.github.com>
2020-09-17 18:36:10 +0530

    Update model.py

--- model.py
+++ model.py
@@ -11,10 +11,11 @@ class roberta_model(nn.Module):
         self.drop = nn.Dropout(p = 0.3)
         self.out = nn.Linear(self.roberta.config.hidden_size, n_classes)
         
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask, labels):
         _, pooled_output = self.roberta(
         input_ids = input_ids,
-        attention_mask = attention_mask
+        attention_mask = attention_mask,
+        labels = labels
         )
         output = self.drop(pooled_output)
         return self.out(output)
